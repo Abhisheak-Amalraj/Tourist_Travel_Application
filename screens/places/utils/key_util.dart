cb3fffab7e3414bbeea62f07a19240b8070aa0f3
Abhisheak Amalraj <abhisheak.amalraj@gmail.com>
2024-04-17 13:34:57 +0100

    Update key_util.dart

--- screens/places/utils/key_util.dart
+++ screens/places/utils/key_util.dart
@@ -1,5 +1,5 @@
 class KeyUtil {
-  static const String GOOGLE_MAP_API_KEY_WEB = "AIzaSyAJ7b-wyZsiuMx4cPYKqiOjePFxpMllZIo";
+  static const String GOOGLE_MAP_API_KEY_WEB = "#";
 
-  static const String GOOGLE_MAP_API_KEY_LOCATION = "AIzaSyAAVRFkp6Kb_bucd2swFN_gZal5viaNo8Y";
+  static const String GOOGLE_MAP_API_KEY_LOCATION = "#";
 }
